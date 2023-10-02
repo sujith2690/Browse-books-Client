7fc38e408226f357df02e7cc1fcb4b0f0f2a0b48
Sujith P <sujithp2690@gmail.com>
2023-10-02 09:50:50 +0530

    add book component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {  Route, Routes } from 'react-router-dom'
 import Auth from './pages/Auth/Auth'
 import Home from './pages/Home'
 import SinglePdt from './pages/SinglePdt'
+import AddBook from './pages/AddBook'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/login" element={<Auth />} />
         <Route path="/signUp" element={<Auth />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/addBook" element={<AddBook />} />
         <Route path="/book/:id" element={<SinglePdt />} />
       </Routes>
     </>
